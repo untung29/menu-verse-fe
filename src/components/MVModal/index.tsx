ae09736d216f880047983e0709a06062c1c7c84f
Untung <u.untung0029@gmail.com>
2024-11-27 15:31:02 +0800

    Styling modal done

--- src/components/MVModal/index.tsx
+++ src/components/MVModal/index.tsx
@@ -1,5 +1,6 @@
-import { Modal, Image, Text } from "@mantine/core";
+import { Modal, Image, Text, Box } from "@mantine/core";
 import { MVModalProps } from "./type";
+import classes from "./MVModal.module.css";
 
 const MVModal = ({
   opened,
@@ -9,10 +10,18 @@ const MVModal = ({
   close,
 }: MVModalProps) => {
   return (
-    <Modal opened={opened} onClose={close} title="Mrs.kueh">
-      <Text>{label}</Text>
-      {description}
-      <Image src={imgSrc} />
+    <Modal
+      opened={opened}
+      onClose={close}
+      title={<Text fw={600}>{label}</Text>}
+      centered
+      size="75%"
+    >
+      <Box className={classes.container}>
+        <Image className={classes.modalImg} src={imgSrc} />
+
+        <Text>{description}</Text>
+      </Box>
     </Modal>
   );
 };
