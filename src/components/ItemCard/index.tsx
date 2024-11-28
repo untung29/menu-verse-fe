e7161c9a0731c3e9ff820279d866b4c2885df909
Untung <u.untung0029@gmail.com>
2024-11-28 17:07:43 +0800

    Add Lineclamp

--- src/components/ItemCard/index.tsx
+++ src/components/ItemCard/index.tsx
@@ -1,5 +1,6 @@
 import { Card, Text, Image, Group, Button, Box } from "@mantine/core";
 import { ItemCardProps } from "./type";
+import classes from "./ItemCard.module.css";
 
 const ItemCard = ({
   label,
@@ -9,18 +10,25 @@ const ItemCard = ({
   onClick,
 }: ItemCardProps) => {
   return (
-    <Card shadow="lg" padding="lg" w="300" withBorder>
+    <Card shadow="lg" padding="lg" w="300" h="515" withBorder>
       <Card.Section>
         <Image src={imgUrl} alt={description} />
       </Card.Section>
 
       <Box py="sm">
-        <Text ta="left" tt="uppercase" fw={600}>
-          {label}
-        </Text>
-        <Text ta="left" fw={400}>
-          {description}
-        </Text>
+        <Box>
+          <Text ta="left" tt="uppercase" fw={600} lineClamp={1}>
+            {label}
+          </Text>
+          <Text
+            ta="left"
+            fw={400}
+            className={classes.descriptionText}
+            lineClamp={3}
+          >
+            {description}
+          </Text>
+        </Box>
 
         <Group pt="lg" justify="space-between">
           <Text ta="left" fw={600}>
