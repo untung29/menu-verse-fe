60533e3b5cbd81ae925e4267433e4c4cb1c11292
Untung <u.untung0029@gmail.com>
2024-11-29 16:34:17 +0800

    Tabs responsiveness

--- src/components/MenuTabs/index.tsx
+++ src/components/MenuTabs/index.tsx
@@ -1,17 +1,40 @@
 import { Tabs } from "@mantine/core";
 import { MenuType } from "./type";
 import MenuTabPanel from "../MenuTabPanel";
+import { useMediaQuery } from "@mantine/hooks";
+import classes from "./MenuTabs.module.css";
 
 const MenuTabs = ({ menus }: { menus: Array<MenuType> }) => {
+  const isTabletScreen = useMediaQuery(`(max-width: 998px)`);
   return (
-    <Tabs color="red" defaultValue={menus[0].id} orientation="vertical">
+    <Tabs
+      color="red"
+      defaultValue={menus[0].id}
+      orientation={isTabletScreen ? "horizontal" : "vertical"}
+      inverted={isTabletScreen}
+      classNames={{
+        list: isTabletScreen ? classes.tabListTablet : classes.tabList,
+      }}
+    >
       <Tabs.List>
         {menus.map((menu) => {
-          return (
+          return [
             <Tabs.Tab key={menu.id} value={menu.id}>
               {menu.label}
-            </Tabs.Tab>
-          );
+            </Tabs.Tab>,
+            <Tabs.Tab key={"1"} value={"1"}>
+              {menu.label}
+            </Tabs.Tab>,
+            <Tabs.Tab key={"2"} value={"2"}>
+              {menu.label}
+            </Tabs.Tab>,
+            <Tabs.Tab key={"2"} value={"2"}>
+              {menu.label}
+            </Tabs.Tab>,
+            <Tabs.Tab key={"2"} value={"2"}>
+              {menu.label}
+            </Tabs.Tab>,
+          ];
         })}
       </Tabs.List>
 
